9b564c2c7c8af0ebffefc36fcfcbcfc207191f86
Yavuz OKTAY <yavuzdark34@gmail.com>
2019-10-28 19:01:48 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Yavuz OKTAY on 28.10.2019.
+//  Copyright © 2019 Yavuz OKTAY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
